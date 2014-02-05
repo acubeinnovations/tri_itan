17650aef0f71c6f2757b0a37019386bd74e41fd6
Nijo Joseph <nijojoseph@acube.co>
2014-02-05 13:21:54 +0530

    set fixed position

--- js/jquery.contactus.responsive.js
+++ js/jquery.contactus.responsive.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
 		if(window_width<='1023'){
 		var appDiv1_width=(window_width*75)/100;
 		var appDiv1_height=(window_height*42)/100;
-		$('.address').css("padding-left",15);
-		$('.address').css("padding-right",10);
+		//$('.address').css("padding-left",15);
+		//$('.address').css("padding-right",10);
 		}else{
 		if (window_width>'1024' && window_width<'1330')
 		{
@@ -27,26 +27,26 @@ $(document).ready(function(){
 		var contact_box_1_width=(appDiv1_width*50)/100;
 		var contact_box_1_height=appDiv1_height;
 		var contact_form_width=(appDiv1_width*35)/100;
-		var table_width=contact_form_width-40;
+		//var table_width=contact_form_width-40;
 		//var table_height=contact_box_1_height-30;
 		var address_width=(contact_box_1_width*40)/100;
 		var address_height=(contact_box_1_height*50)/100;
 		var textbox_size=contact_form_width-50;
 		
-		$('#apDiv1').css("width",appDiv1_width);
-		$('#apDiv1').css("height",appDiv1_height);
-		$('#contact_box_1').css("width",contact_box_1_width);
-		$('#contact_box_1').css("height",contact_box_1_height);
-		$('#contact_form').css("width",contact_form_width);
-		$('#contact_form').css("height",contact_box_1_height);
-		$('#contact-table').css("width",table_width);
+		//$('#apDiv1').css("width",appDiv1_width);
+		//$('#apDiv1').css("height",appDiv1_height);
+		//$('#contact_box_1').css("width",contact_box_1_width);
+		//$('#contact_box_1').css("height",contact_box_1_height);
+		//$('#contact_form').css("width",contact_form_width);
+		//$('#contact_form').css("height",contact_box_1_height);
+		//$('#contact-table').css("width",table_width);
 		//$('#contact-table').css("height",table_height);
-		$('.address').css("width",address_width);
-		$('.address').css("height",address_height);
-		$('#name').css("width",textbox_size);
-		$('#company').css("width",textbox_size);
-		$('#email').css("width",textbox_size);
-		$('#message').css("width",textbox_size);
+		//$('.address').css("width",address_width);
+		//$('.address').css("height",address_height);
+		//$('#name').css("width",textbox_size);
+		//$('#company').css("width",textbox_size);
+		//$('#email').css("width",textbox_size);
+		//$('#message').css("width",textbox_size);
 	}
 	responsive();
 	var zoom = $( window ).width();
